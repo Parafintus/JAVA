fdd17d5156e9d84fea335f6da35f1d73e366481c
Nikita <ba999@mail.ru>
2023-06-23 18:36:29 +0300

    Added HW.2

---
+++ HW.2/Task2.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
+//Реализуйте алгоритм сортировки пузырьком числового массива,
+//        результат после каждой итерации запишите в лог-файл.
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger(Task2.class.getName());
+        logger.setLevel(Level.FINE);
+        logger.setUseParentHandlers(false);
+
+        try {
+            FileHandler fh = new FileHandler("log_bubble_sort.xml");
+            logger.addHandler(fh);
+
+            XMLFormatter xml = new XMLFormatter();
+            fh.setFormatter(xml);
+
+            BubbleSortWithLog(logger);
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void BubbleSortWithLog(Logger logger) {
+
+        int[] mas = { 11, 100, 45, 7, 14, 99, 1, 0, 509, 3, 78, 22 };
+
+        boolean isSorted = false;
+        int buf;
+
+        System.out.println(mas);
+
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
+                    isSorted = false;
+                    logger.info(mas[i] + " - " + mas[i + 1]);
+
+                    buf = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buf;
+                    // Logging();
+                }
+            }
+        }
+        System.out.println("----------------Сортировка пузырьком----------------");
+        System.out.println(Arrays.toString(mas));
+    }
+}
